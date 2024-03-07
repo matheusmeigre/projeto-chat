601a94bda2a806d33a85db400f70117e4194df2f
matheusmeigre <142261154+matheusmeigre@users.noreply.github.com>
2024-03-07 17:27:34 -0300

    att

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -84,7 +84,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://chat-service-d3km.onrender.com")
+    websocket = new WebSocket("ws://chat-front-77lg.onrender.com")
     websocket.onmessage = proccessMessage
 }
 
