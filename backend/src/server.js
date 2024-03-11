c0bc0b0d9665b369e0fb5b6bd2a93495f1a161e5
matheusmeigre <142261154+matheusmeigre@users.noreply.github.com>
2024-03-11 04:03:39 -0300

    envio de arquivo de audio

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,69 @@
 const { WebSocketServer } = require("ws")
 const dotenv = require("dotenv")
+const fs = require('fs')
 
 dotenv.config()
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
-wss.on("connection", (ws) => {
-    ws.on("error", console.error)
+websocket.onmessage = function(event) {
+    const data = JSON.parse(event.data);
+  
+    // Verificar se a mensagem contém o nome do arquivo de áudio
+    if (data.audioFileName) {
+      const audioUrl = `wss://chat-backend-pmzy.onrender.com${data.audioFileName}`;
+  
+      // Criar um elemento de áudio e reproduzir o áudio
+      const audioElement = new Audio(audioUrl);
+      audioElement.controls = true;
+      document.body.appendChild(audioElement);
+    }
+  };
+  
+  // Exemplo de como enviar um arquivo de áudio para o servidor
+  function sendAudioFileToServer(file) {
+    const reader = new FileReader();
+  
+    reader.onload = function() {
+      const audioData = reader.result;
+  
+      const message = {
+        audio: audioData
+      };
+  
+      websocket.send(JSON.stringify(message));
+    };
+  
+    reader.readAsBinaryString(file);
+  }
 
-    ws.on("message", (data) => {
-        wss.clients.forEach((client) => client.send(data.toString()))
-    })
-
-    console.log("client connected")
-})
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+      try {
+        const parsedMessage = JSON.parse(message);
+  
+        // Verificar se a mensagem contém áudio
+        if (parsedMessage.audio) {
+          const audioData = parsedMessage.audio;
+          const fileName = `audio_${Date.now()}.wav`;
+  
+          // Salvar os dados de áudio em um arquivo
+          fs.writeFile(fileName, audioData, 'binary', (err) => {
+            if (err) {
+              console.error('Erro ao salvar o arquivo de áudio:', err);
+              return;
+            }
+            console.log('Arquivo de áudio salvo:', fileName);
+  
+            // Enviar confirmação ao cliente, se necessário
+            // ws.send(JSON.stringify({ audioSaved: true }));
+          });
+        } else {
+          // Lidar com a mensagem de texto
+          console.log('Recebeu mensagem de texto:', parsedMessage);
+        }
+      } catch (error) {
+        console.error('Erro ao analisar mensagem JSON:', error);
+      }
+    });
+  });
